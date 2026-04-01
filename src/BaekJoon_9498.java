7ae19a75f70025e675d260513b61f09fee9ada0f
daehyuk1231 <daehyuk1231@naver.com>
2026-04-01 10:11:51 +0900

    first commit

---
+++ src/BaekJoon_9498.java
@@ -0,0 +1,16 @@
+// 문제: 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+import java.util.Scanner;
+
+public class BaekJoon_9498 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (100 >= num && num >= 90) System.out.println("A");
+        else if (89 >= num && num >= 80) System.out.println("B");
+        else if (79 >= num && num >= 70) System.out.println("C");
+        else if (69 >= num && num >= 60) System.out.println("D");
+        else System.out.println("F");
+    }
+}

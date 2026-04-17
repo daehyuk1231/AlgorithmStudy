df9764ab8c021683f151f693b96e7776c3215c34
daehyuk1231 <daehyuk1231@naver.com>
2026-04-17 09:35:25 +0900

    1929

---
+++ src/방대혁_BaekJoon_1929.java
@@ -0,0 +1,27 @@
+// 문제: M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+import java.util.Scanner;
+
+public class 방대혁_BaekJoon_1929 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int[] A = new int[N + 1];
+        for (int i = 2; i <= N; i++) {
+            A[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (A[i] == 0) continue;
+            for (int j = i + i; j <= N; j = j + i) {
+                A[j] = 0;
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (A[i] != 0) {
+                System.out.println(A[i]);
+            }
+        }
+    }
+}
